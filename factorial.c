df5674c678db93e7a1589851f0bc98a9df48f53a
Qila Nguyen <otokhung1805@gmail.com>
2022-01-27 22:15:05 +0700

    sprintf, atoi

--- factorial.c
+++ factorial.c
@@ -3,21 +3,20 @@
 #include<stdio.h>
 #include<cstring>
 
-int factInt (const int aNumber) {
-    if (aNumber <= 1) {
-        return 1;
+char *factorial (const int aNumber) {
+    int temp = aNumber;
+    int result = 1;
+    while (temp > 1) {
+        result *= temp;
+        temp--;
     }
+    char *res = malloc(4*sizeof(*res));
 
-    return aNumber*factInt(aNumber-1);
-}
-
-char *factorial (int result) {
-    int size = log10(result)+1;
-    char *res = malloc(size*sizeof(*res));
-
-    for (int i=1;i<=size;i++) {
-        res[size-i] = result%10 + '0';
-        result /= 10;
+    temp = 0;
+    sprintf(res,"%d",result);
+    while (res[temp]) {
+        printf("%c ", res[temp]);
+        temp++;
     }
 
     return res;
--- main.c
+++ main.c
@@ -1,21 +1,7 @@
 #include "readline.h"
 #include "factorial.h"
 #include<stdio.h>
-#include<stdbool.h>
-
-int convertToNumber (char *str) {
-    int i = 0;
-    int res = 0;
-    bool neg = (str[0] == '-');
-    if(neg) i = 1;
-    while (str[i]) {
-        res = res*10 + (str[i] - '0');
-        i++;
-    }
-    if (neg) res *= -1;
-
-    return res;
-}
+#include<stdlib.h>
 
 int main () {
     char *text = (char*)malloc(4*sizeof(*text));
@@ -23,10 +9,10 @@ int main () {
     scanf("%[^\n]s",text);
     printf("You typed a line: %s \n", text);
     if (read_line(text) == 1) {
-        printf("%d \n", convertToNumber(text));
+        printf("%d \n", atoi(text));
         // int num = convertToNumber;
         // printf("%d", factInt(convertToNumber(text)));
-        factorial(factInt(convertToNumber(text)));
+        factorial(atoi(text));
     }
     else {
         printf("%d",-1);
